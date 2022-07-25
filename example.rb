fe5d46adc7ea1a88a1070352f19f73dee3cd21ca
Kay <kard@hotmail.co.uk>
2022-07-25 14:20:15 +0100

    Updated example.rb to reflect the acceptance criteria

--- example.rb
+++ example.rb
@@ -1,8 +1,10 @@
 require ('./lib/bank_account')
 require ('./lib/statement')
 
-account = BankAccount.new(Kernel)
+account = BankAccount.new(Kernel, Statement)
 
-account.deposit(100, "24/07/2022")
+account.deposit(1000, "10/01/2023")
+account.deposit(2000, "13/01/2023")
+account.withdraw(500, "14/01/2023")
 
 account.show_statement
