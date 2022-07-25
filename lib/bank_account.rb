3f84e93cb14efb7375d4239019737f2b4f27dfd3
Kay <kard@hotmail.co.uk>
2022-07-25 11:05:36 +0100

    Shows a statement with a deposit of 100 if the deposit method has been called

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,15 @@ class BankAccount
   def initialize(statement)
   end
 
+  def deposit(amount, date)
+    @deposit = true
+  end
+
   def show_statement()
-    "date || credit || debit || balance"
+    if @deposit == true
+      return "date || credit || debit || balance\n24/07/2022 || || 100.00 || 100.00"
+    else
+      return "date || credit || debit || balance"
+    end
   end
 end
--- spec/account_integration_spec.rb
+++ spec/account_integration_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe 'account integration' do
       expect(account.show_statement).to eq "date || credit || debit || balance"
     end
   end
+
+  context 'user makes a deposit' do
+    it 'shows the deposit on the statement' do
+      statement = Statement.new
+      account = BankAccount.new(statement)
+      account.deposit(100, '24/07/2022')
+      expect(account.show_statement).to eq "date || credit || debit || balance\n24/07/2022 || || 100.00 || 100.00"
+    end
+  end
 end
