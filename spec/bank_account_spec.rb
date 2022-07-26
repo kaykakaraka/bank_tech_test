183b771f97d7049366f33fc5a401b95c8e3150d7
Kay <kard@hotmail.co.uk>
2022-07-26 12:01:10 +0100

    Account passes different dates to the date checker: unit test

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -53,4 +53,19 @@ RSpec.describe BankAccount do
       account.withdraw(50, '27/07/2022')
     end
   end
+
+  context 'deposits on two different dates are made' do
+    it 'passes both dates to the date checker' do
+      io = double :io
+      fake_statement_class = double(:statement, new: fake_statement = double(:statement_object))
+      fake_date_checker = double(:date_checker, new: fake_date_checker1 = double(:date_checker_object))
+      expect(fake_statement).to receive(:add_deposit).with(100, '27/07/2022', 100)
+      expect(fake_statement).to receive(:add_withdrawal).with(50, '28/10/2023', 50)
+      expect(fake_date_checker1).to receive("check_date").with('27/07/2022')
+      expect(fake_date_checker1).to receive("check_date").with('28/10/2023')
+      account = BankAccount.new(io, fake_statement_class, fake_date_checker)
+      account.deposit(100, '27/07/2022')
+      account.withdraw(50, '28/10/2023')
+    end
+  end
 end
