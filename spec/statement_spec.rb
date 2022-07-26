3c25fe5689a203dc9aa02675d7271f98575bcea1
Kay <kard@hotmail.co.uk>
2022-07-26 11:27:53 +0100

    Shows a deposit of a different amount on the statement "

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,16 +9,22 @@ RSpec.describe Statement do
   end
 
   context 'when a deposit has been made' do
-    it 'returns the deposit on the statement' do
+    it 'shows the deposit on the statement' do
       statement = Statement.new
       statement.add_deposit(100,'24/07/2022', 100)
       expect(statement.statement).to eq ['date || credit || debit || balance', '24/07/2022 || || 100.00 || 100.00']
     end
 
-    it 'returns a deposit with a different date on the statement' do
+    it 'shows a deposit with a different date on the statement' do
       statement = Statement.new
       statement.add_deposit(100,'25/07/2022', 100)
       expect(statement.statement).to eq ['date || credit || debit || balance', '25/07/2022 || || 100.00 || 100.00']
+    end
+
+    it 'shows a deposit with a different amount on the statement' do
+      statement = Statement.new
+      statement.add_deposit(150,'25/07/2022', 150)
+      expect(statement.statement).to eq ['date || credit || debit || balance', '25/07/2022 || || 150.00 || 150.00']
     end
   end
 end
