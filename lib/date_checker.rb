8713e0b407552e9c1aeded5eda7bec252483935f
Kay <kard@hotmail.co.uk>
2022-07-26 09:40:45 +0100

    Refactored DateChecker by moving separate(date) to the main method that calls the other methods

--- lib/date_checker.rb
+++ lib/date_checker.rb
@@ -1,20 +1,19 @@
 class DateChecker
   def check_date(date)
+    separate(date)
     fail 'Invalid Date' unless valid_date?(date)
     fail 'Invalid Date: must be later than your most recent transaction' unless
-      most_recent_date?(date)
+      most_recent_date?
     @most_recent_date = Date.new(@year.to_i, @month.to_i, @day.to_i)
   end
 
   def valid_date?(date)
-    separate(date)
     return false if date.include?('-') || @year.length != 4 
     Date.valid_date?(@year.to_i, @month.to_i, @day.to_i) 
   end
 
-  def most_recent_date?(date)
+  def most_recent_date?
     return true unless @most_recent_date 
-    separate(date)
     result = @most_recent_date <=> Date.new(@year.to_i, @month.to_i, @day.to_i)
     result != 1 
   end
