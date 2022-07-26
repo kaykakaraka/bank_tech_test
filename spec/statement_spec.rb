bc8927f42676d15585022b34747acc45aa9d0498
Kay <kard@hotmail.co.uk>
2022-07-26 11:24:13 +0100

    Returns a single deposit on the statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Statement do
   end
 
   context 'when a deposit has been made' do
-    xit 'returns the deposit on the statement' do
+    it 'returns the deposit on the statement' do
       statement = Statement.new
       statement.add_deposit(100,'24/07/2022', 100)
       expect(statement.statement).to eq ['date || credit || debit || balance', '24/07/2022 || || 100.00 || 100.00']
